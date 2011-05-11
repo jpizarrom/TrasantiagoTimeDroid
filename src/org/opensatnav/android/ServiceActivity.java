0b8f8358346c793b73c6c2ee4ce9f7b0c10557ff
Juan Pizarro <jpizarro@doingit.cl>
2011-05-11 18:41:27 -0400

    ss

---
+++ src/org/opensatnav/android/ServiceActivity.java
@@ -0,0 +1,36 @@
+package org.opensatnav.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+
+public class ServiceActivity extends Activity {
+	final Activity activity = this;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.web);
+		WebView webView = (WebView) findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        String url = getIntent().getStringExtra("url");
+        String params = getIntent().getStringExtra("params");
+        webView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress)
+            {
+                activity.setTitle("Loading...");
+                activity.setProgress(progress * 100);
+ 
+                if(progress == 100)
+                    activity.setTitle(R.string.app_name);
+                Log.i(OpenSatNavConstants.LOG_TAG, view.getUrl());
+            }
+        });
+        
+        webView.loadUrl(url);
+	}
+
+}
